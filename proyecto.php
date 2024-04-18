9094c8b80fc7d62096f0d580900ae23b77d88456
tomaserrezarret <tomis34.e@gmail.com>
2024-04-17 21:45:48 -0300

    aca esta todo

--- proyecto.php
+++ proyecto.php
@@ -1,3 +1,11 @@
 <?php
-
+ 
+ if(isset($_POST['ingresar'])){
+    $usuario = $_POST['usuario'];
+     if($usuario == 'roberto'){
+        echo "es correcto"
+     else
+        echo "no es correcto"
+    }
+  }
 ?>
